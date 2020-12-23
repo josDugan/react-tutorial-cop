4e3c56b7adde762b94fc006f1d94bb83787d72fd
josDugan <josdugan@gmail.com>
2020-12-23 18:53:50 -0500

    Implement post handler Closes #4.

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,20 +3,17 @@ const router = express.Router();
 
 const connectionPoolService = require('../database/connection-pool');
 
-router.get('/', function (req, res, next) {
+router.post('/', function(req, res) {
+    console.log('post body', req.body);
 
-    const book = {
-        'author': 'Charles Dickens',
-        'title': 'Great Expectations',
-        'published': '1861-09-01'
-    };
-
-    connectionPoolService.getPool().query('insert into books set ?', book, (err, result) => {
+    connectionPoolService.getPool().query('insert into books set ?', req.body, (err, result) => {
         if (err) throw err;
 
         console.log(result);
     });
+});
 
+router.get('/', function (req, res) {
     res.send('here are some other books!');
 });
 
