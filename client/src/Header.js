c85ac78d72952f3d930a603e26e7948ed5a7c113
josDugan <josdugan@gmail.com>
2020-12-24 16:14:23 -0500

    Retrieve books and display list to user. Closes #8. Cloes #9.

---
+++ client/src/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <ul className="header-left">
+                <li className="logo"><a href="/">Book Library</a></li>
+            </ul>
+
+            <ul className="header-right">
+                <li><a href="/">Create</a></li>
+                <li><a href="/">View</a></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
--- server/app.js
+++ server/app.js
@@ -6,9 +6,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var booksRouter = require('./routes/books');
+var cors = require('cors')
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
