378d25e331f0759fadeb7784b26cf28994ae1555
josDugan <josdugan@gmail.com>
2020-12-23 19:23:24 -0500

    Add stubs for get, put, delete. Closes #5

--- server/routes/books.js
+++ server/routes/books.js
@@ -3,18 +3,37 @@ const router = express.Router();
 
 const connectionPoolService = require('../database/connection-pool');
 
+
 router.post('/', function(req, res) {
     console.log('post body', req.body);
-
+    
     connectionPoolService.getPool().query('insert into books set ?', req.body, (err, result) => {
         if (err) throw err;
-
+        
         console.log(result);
     });
 });
 
 router.get('/', function (req, res) {
     res.send('here are some other books!');
+});
+
+router.get('/:id', function(req, res) {
+    console.log('GET', 'ID', req.params.id);
+    
+    res.sendStatus(200);
+});
+
+router.put('/:id', function(req, res) {
+    console.log('PUT', 'ID', req.params.id, 'BODY', req.body);
+
+    res.sendStatus(200);
+});
+
+router.delete('/:id', function(req, res) {
+    console.log('DELETE', 'ID', req.params.id);
+
+    res.sendStatus(200);
 });
 
 module.exports = router;
