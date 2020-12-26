e5532483a1bc050b976e30f4c249b78c73ba76b9
josDugan <josdugan@gmail.com>
2020-12-25 22:03:31 -0500

    Add book form. Closes #11.

--- client/src/Book.js
+++ client/src/Book.js
@@ -5,8 +5,16 @@ function Book() {
 console.log('env', process.env);
 
     return (
-        <div id="book-message">
-            Hello I'm a book
+        <div>
+            <form>
+                <label htmlFor="author">Author:</label>
+                <input type="text" name="author" id="author"/>
+                <label htmlFor="title">Title:</label>
+                <input type="text" name="title" id="title"/>
+                <label htmlFor="publishedr">Published:</label>
+                <input type="text" name="published" id="published"/>
+                <input type="submit" value="Save"/>
+            </form>
         </div>
     );
 }
