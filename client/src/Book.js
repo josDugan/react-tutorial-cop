ea4aeaff4f27df9afb0813f5a389e4cda6c668fd
josDugan <josdugan@gmail.com>
2020-12-25 22:33:24 -0500

    Add book form controls and submission. Closes #13.

--- client/src/Book.js
+++ client/src/Book.js
@@ -1,22 +1,69 @@
+import React from 'react';
+import axios from 'axios';
+
 import './Book.css';
 
-function Book() {
+class Book extends React.Component {
 
-console.log('env', process.env);
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>
-            <form>
-                <label htmlFor="author">Author:</label>
-                <input type="text" name="author" id="author"/>
-                <label htmlFor="title">Title:</label>
-                <input type="text" name="title" id="title"/>
-                <label htmlFor="publishedr">Published:</label>
-                <input type="text" name="published" id="published"/>
-                <input type="submit" value="Save"/>
-            </form>
-        </div>
-    );
+        this.state = {
+            author: '',
+            title: '',
+            published: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        let { author, title, published } = this.state;
+
+        published += '-01-01';
+
+        const book = {
+            author: author,
+            title: title,
+            published: published
+        }
+
+        axios.post(process.env.REACT_APP_SERVER_URL, book)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="author">Author:</label>
+                    <input value={this.state.author} onChange={this.handleChange} type="text" name="author" id="author" />
+                    <label htmlFor="title">Title:</label>
+                    <input value={this.state.titler} onChange={this.handleChange} type="text" name="title" id="title" />
+                    <label htmlFor="published">Published:</label>
+                    <input value={this.state.published} onChange={this.handleChange} type="text" name="published" id="published" />
+                    <input type="submit" value="Save" />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Book;
