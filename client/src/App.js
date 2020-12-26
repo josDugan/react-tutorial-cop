ba2bc270e256d78107afc9cbdb3ec82dedf89c37
josDugan <josdugan@gmail.com>
2020-12-25 21:46:37 -0500

    Add router. Closes #10.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Header from './Header';
 import BookLibrary from './BookLibrary';
+import Book from './Book';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <BookLibrary />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <BookLibrary />
+          </Route>
+          <Route path="/create">
+            <Book/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,16 @@
+import {Link} from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return (
         <div className="header">
             <ul className="header-left">
-                <li className="logo"><a href="/">Book Library</a></li>
+                <li className="logo"><Link to="/">Book Library</Link></li>
             </ul>
 
             <ul className="header-right">
-                <li><a href="/">Create</a></li>
-                <li><a href="/">View</a></li>
+                <li><Link to="/create">Create</Link></li>
+                <li><Link to="/">View</Link></li>
             </ul>
         </div>
     );
